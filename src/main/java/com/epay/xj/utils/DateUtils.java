0fe11cb8875d0392c72abc1a541b0dfa0bfa0b97
LZG <LZG@DESKTOP-TUD0U2K>
2018-07-04 00:23:50 +0800

    添加获取某日期X个月前的日期的方法

--- src/main/java/com/epay/xj/utils/DateUtils.java
+++ src/main/java/com/epay/xj/utils/DateUtils.java
@@ -9,75 +9,113 @@ import java.util.List;
 
 public class DateUtils {
 
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat();
-	
-	/**
-	 * yyyyMMdd时间格式
-	 */
-	private static final String yyyyMMdd = "yyyyMMdd";
-	
-	
-	/**
-	 * @Title: yyyyMMdd 
-	 * @Description: 
-	 * @param: @param dateText
-	 * @param: @return    
-	 * @return: Date    
-	 * @author yanghf
-	 * @Date 2018年6月28日 上午11:43:35
-	 */
-	public synchronized static Date yyyyMMddToDate(String dateText) {
-		dateFormat.applyPattern(DateUtils.yyyyMMdd);
-		try {
-			return dateFormat.parse(dateText);
-		} catch (java.text.ParseException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	/**
-	 * 
-	 * @Description:当前时间加上指定天数后的时间
-	 * 
-	 */
-	public static Date currentAddDays(Date date, int days) {
-		Calendar ca = Calendar.getInstance();
-		ca.setTime(date);
-		ca.add(Calendar.DATE, days);
-		return ca.getTime();
-	}
-	
-	/**
-	 * @Title: yyyyMMdd 
-	 * @Description: 时间转换
-	 * @param: @param date
-	 * @return: String    
-	 * @author yanghf
-	 * @Date 2018年5月10日 下午2:17:18
-	 */
-	public synchronized static String yyyyMMddToString(Date date){
-		dateFormat.applyPattern(DateUtils.yyyyMMdd);
-		return dateFormat.format(date);
-	}
-	public static String[] getOneYearDate(String date,int days){
-		List<String> dateList = new ArrayList<String>();
-		Date cDate = yyyyMMddToDate(date);
-		dateList.add(yyyyMMddToString(cDate));
-		for(int i=1;i<days;i++){
-			Date eDate = currentAddDays(cDate,-i);
-			dateList.add(yyyyMMddToString(eDate));
-		}
-		String[] arrayDate = new String[dateList.size()];
-		return dateList.toArray(arrayDate);
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] arr =  getOneYearDate("20180401",365);
-		for (String string : arr) {
-			System.out.println(string);
-		}
-	}
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat();
 
-	
+    /**
+     * yyyyMMdd时间格式
+     */
+    private static final String yyyyMMdd = "yyyyMMdd";
+
+    /**
+     * @Title: yyyyMMdd
+     * @Description:
+     * @param: @param dateText
+     * @param: @return
+     * @return: Date
+     * @author yanghf
+     * @Date 2018年6月28日 上午11:43:35
+     */
+    public synchronized static Date yyyyMMddToDate(String dateText) {
+        dateFormat.applyPattern(DateUtils.yyyyMMdd);
+        try {
+            return dateFormat.parse(dateText);
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @Description:当前时间加上指定天数后的时间
+     */
+    public static Date currentAddDays(Date date, int days) {
+        Calendar ca = Calendar.getInstance();
+        ca.setTime(date);
+        ca.add(Calendar.DATE, days);
+        return ca.getTime();
+    }
+
+    /**
+     * @Title: yyyyMMdd
+     * @Description: 时间转换
+     * @param: @param date
+     * @return: String
+     * @author yanghf
+     * @Date 2018年5月10日 下午2:17:18
+     */
+    public synchronized static String yyyyMMddToString(Date date) {
+        dateFormat.applyPattern(DateUtils.yyyyMMdd);
+        return dateFormat.format(date);
+    }
+
+    public static String[] getOneYearDate(String date, int days) {
+        List<String> dateList = new ArrayList<String>();
+        Date cDate = yyyyMMddToDate(date);
+        dateList.add(yyyyMMddToString(cDate));
+        for (int i = 1; i < days; i++) {
+            Date eDate = currentAddDays(cDate, -i);
+            dateList.add(yyyyMMddToString(eDate));
+        }
+        String[] arrayDate = new String[dateList.size()];
+        return dateList.toArray(arrayDate);
+    }
+
+    /**
+     * @Description: 获取某个日期N个月前的日期
+     * @param date
+     * @param monthAmount
+     * @return 
+     * @author LZG
+     * @date 2018年07月03日
+     */
+    public static synchronized String getDateOfXMonthsAgo(String date, int monthAmount) {
+
+        Date currentDate = yyyyMMddToDate(date);
+        // 获得一个日历的实例
+        Calendar c = Calendar.getInstance();
+        // 设置日历时间
+        c.setTime(currentDate);
+        // 在日历的月份上减去N个月
+        c.add(Calendar.MONTH, -monthAmount);
+        
+        return DateUtils.yyyyMMddToString(c.getTime());
+    }
+
+    public static void main(String[] args) {
+        //test1();
+        test2();
+    }
+    
+    public static void test1() {
+        String[] arr = getOneYearDate("20180401", 365);
+        for (String string : arr) {
+            System.out.println(string);
+        }
+    }
+    
+    public static void test2() {
+        //12个月前的日期
+        String str_12m_before = getDateOfXMonthsAgo("20180631", 12);
+        System.out.println("20180631, 12个月前的日期是：" + str_12m_before);
+        
+        //6个月前的日期
+        String str_6m_before = getDateOfXMonthsAgo("20180631", 6);
+        System.out.println("20180631, 6个月前的日期是：" + str_6m_before);
+        
+        //3个月前的日期
+        String str_3m_before = getDateOfXMonthsAgo("20180631", 3);
+        System.out.println("20180631, 3个月前的日期是：" + str_3m_before);
+        
+    }
+
 }

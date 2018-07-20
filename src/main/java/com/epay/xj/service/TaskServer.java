b349d971c79e17a7c2b9a61df7fdaa47ad178ce1
comingcn2104 <yanghai.feng1024@163.com>
2018-07-20 09:24:57 +0800

    修改扣款类指标统计

--- src/main/java/com/epay/xj/service/TaskServer.java
+++ src/main/java/com/epay/xj/service/TaskServer.java
@@ -26,7 +26,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.epay.xj.domain.BindCardLog;

aaa4dd7dccfcb3ecdf2056f06b4b9911a9ae6546
comingcn2104 <yanghai.feng1024@163.com>
2018-07-21 11:40:59 +0800

    修改配置文件

--- src/main/java/com/epay/xj/EpayApplication.java
+++ src/main/java/com/epay/xj/EpayApplication.java
@@ -4,7 +4,6 @@ package com.epay.xj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 @SpringBootApplication
 @EnableConfigurationProperties
 public class EpayApplication {

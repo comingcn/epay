d2d46e5985410870495a0086f36df24b7abbfc10
comingcn2104 <yanghai.feng1024@163.com>
2018-08-07 21:00:40 +0800

    add

---
+++ src/main/java/com/epay/xj/dao/ICertDao.java
@@ -0,0 +1,17 @@
+package com.epay.xj.dao;
+
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.epay.xj.domain.CertNo;
+
+public interface ICertDao {
+	@Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT,timeout=36000,rollbackFor=Exception.class)
+	public void insert(CertNo o);
+	
+	public List<CertNo> findAll();
+}
